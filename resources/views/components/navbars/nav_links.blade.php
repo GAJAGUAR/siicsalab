34d90eac1af7a44ee755f0c8648cd9e6a04110a6
G.A.JAGUAR <dev.gajaguar@gmail.com>
2019-07-22 14:22:07 -0500

    Created quick links on 'app' view
    Quick access for 'Catalogue' and 'New'.

--- resources/views/components/navbars/nav_links.blade.php
+++ resources/views/components/navbars/nav_links.blade.php
@@ -1,2 +1,53 @@
-<ul class="navbar-nav mr-auto">
-</ul>
+<div class="navbar-nav mr-auto">
+  @component('components.dropdowns.dropdown')
+    @slot('id')
+      {{ __('dropdown-catalogue') }}
+    @endslot
+
+    @slot('category')
+      {{ __('catálogo') }}
+    @endslot
+
+    @component('components.dropdowns.dropdown_item')
+      @slot('url')
+        {{ route('clients.index') }}
+      @endslot
+
+      {{ __('clientes') }}
+    @endcomponent
+
+    @component('components.dropdowns.dropdown_item')
+      @slot('url')
+        {{ route('works.index') }}
+      @endslot
+
+      {{ __('obras') }}
+    @endcomponent
+  @endcomponent
+
+  @component('components.dropdowns.dropdown')
+    @slot('id')
+      {{ __('dropdown-new') }}
+    @endslot
+
+    @slot('category')
+      {{ __('nuevo') }}
+    @endslot
+
+    @component('components.dropdowns.dropdown_item')
+      @slot('url')
+        {{ route('clients.create') }}
+      @endslot
+
+      {{ __('clientes') }}
+    @endcomponent
+
+    @component('components.dropdowns.dropdown_item')
+      @slot('url')
+        {{ route('works.create') }}
+      @endslot
+
+      {{ __('obras') }}
+    @endcomponent
+  @endcomponent
+</div>
