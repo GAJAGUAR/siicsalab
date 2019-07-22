1968e81b9e0af6a7718c2c596c6dec17f8bc2e54
G.A.JAGUAR <dev.gajaguar@gmail.com>
2019-07-22 14:13:34 -0500

    Created 'works' table
    Modified 'clients' migration only for visual purposes.

--- database/migrations/2019_07_22_133642_create_works_table.php
+++ database/migrations/2019_07_22_133642_create_works_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClientsTable extends Migration
+class CreateWorksTable extends Migration
 {
   /**
    * Run the migrations.
@@ -15,24 +15,30 @@ class CreateClientsTable extends Migration
   {
     Schema::disableForeignKeyConstraints();
 
-    Schema::create('clients', function (Blueprint $table) {
+    Schema::create('works', function (Blueprint $table) {
 
       // DDL
       $table->engine = 'InnoDB';
       $table->charset = 'utf8mb4';
       $table->collation = 'utf8mb4_spanish_ci';
       $table->smallIncrements('id');
-      $table->string('client_name', 150);
-      $table->string('client_nickname', 50);
-      $table->string('client_register', 25)->nullable()->default(null);
-      $table->string('client_location', 250)->nullable()->default(null);
+      $table->unsignedSmallInteger('client_id');
+      $table->string('work_name', 750);
+      $table->string('work_nickname', 50);
+      $table->string('work_location', 250);
       $table->timestamps();
 
       // Indexes
-      $table->unique('client_name');
-      $table->unique('client_nickname');
+      $table->index('client_id');
+      $table->unique('work_nickname');
+
+      // Foreign keys
+      $table->foreign('client_id')
+        ->references('id')->on('clients')
+        ->onDelete('cascade')
+        ->onUpdate('cascade');
     });
-    
+
     Schema::enableForeignKeyConstraints();
   }
 
@@ -45,8 +51,8 @@ class CreateClientsTable extends Migration
   {
     Schema::disableForeignKeyConstraints();
 
-    Schema::dropIfExists('clients');
-
+    Schema::dropIfExists('works');
+    
     Schema::enableForeignKeyConstraints();
   }
 }
