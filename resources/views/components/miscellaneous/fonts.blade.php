84fdb66b5e0775a99019d144d55ec3cafdd08b4d
G.A.JAGUAR <dev.gajaguar@gmail.com>
2019-07-29 00:28:29 -0500

    Turn assets to offline

--- resources/views/components/miscellaneous/fonts.blade.php
+++ resources/views/components/miscellaneous/fonts.blade.php
@@ -1,4 +1,4 @@
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css"
-      integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito">
+{{--<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css"--}}
+{{--      integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">--}}
--- resources/views/components/miscellaneous/scripts.blade.php
+++ resources/views/components/miscellaneous/scripts.blade.php
@@ -1,5 +1,14 @@
+{{-- main app --}}
 <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js" defer></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"
-        defer></script>
+
+{{-- data tables --}}
+<script type="text/javascript" src="{{ asset('js/vendor/datatables.min.js') }}" defer></script>
+{{--<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js" defer></script>--}}
+
+{{-- select 2 --}}
+<script type="text/javascript" src="{{ asset('js/vendor/select2.full.min.js') }}" defer></script>
+{{--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"--}}
+{{--        defer></script>--}}
+
+{{-- secondary app --}}
 <script type="text/javascript" src="{{ asset('js/app.plugins.js') }}" defer></script>
