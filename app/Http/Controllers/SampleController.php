ba9bfda77672adc7cb4ceaf6923c1e411e4c2d32
G.A.JAGUAR <dev.gajaguar@gmail.com>
2019-08-17 12:03:59 -0500

    Adjusted 'sample' controller to new model

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -2,15 +2,9 @@
 
 namespace Sislab\Http\Controllers;
 
-use Sislab\Sample;
+use Sislab\{Bank, Priority, Purpose, Sample, Status, Weather, WorkOrder};
 
-use Illuminate\Http\Request;
-
-use Illuminate\Http\Response;
-
-use Exception;
-
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\{Request, Response};
 
 class SampleController extends Controller
 {
@@ -26,14 +20,7 @@ class SampleController extends Controller
    */
   public function index()
   {
-    $samples = DB::table('samples')
-      ->join('work_orders', 'work_orders.id', '=', 'samples.work_order_id')
-      ->join('works', 'works.id', '=', 'work_orders.work_id')
-      ->join('clients', 'clients.id', '=', 'works.client_id')
-      ->join('purposes', 'purposes.id', '=', 'samples.purpose_id')
-      ->join('statuses', 'statuses.id', '=', 'samples.status_id')
-      ->select('samples.id', 'work_order_id', 'work_nickname', 'client_nickname', 'sample_description', 'purpose_name', 'sample_receipt_date', 'status_name')
-      ->get();
+    $samples = (new Sample)->getSamples();
 
     return view('samples.index', [
       'samples' => $samples
@@ -47,29 +34,17 @@ class SampleController extends Controller
    */
   public function create()
   {
-    $workOrders = DB::table('work_orders')
-      ->select('id')
-      ->get();
+    $workOrders = (new WorkOrder)->getWorkOrders();
 
-    $banks = DB::table('banks')
-      ->select('id', 'bank_name')
-      ->get();
+    $banks = (new Bank)->getBanks();
 
-    $purposes = DB::table('purposes')
-      ->select('id', 'purpose_name')
-      ->get();
+    $purposes = (new Purpose)->getPurposes();
 
-    $weathers = DB::table('weathers')
-      ->select('id', 'weather_name')
-      ->get();
+    $weathers = (new Weather)->getWeathers();
 
-    $priorities = DB::table('priorities')
-      ->select('id', 'priority_name')
-      ->get();
+    $priorities = (new Priority)->getPriorities();
 
-    $statuses = DB::table('statuses')
-      ->select('id', 'status_name')
-      ->get();
+    $statuses = (new Status)->getStatuses();
 
     return view('samples.create', [
       'workOrders' => $workOrders,
@@ -89,63 +64,11 @@ class SampleController extends Controller
    */
   public function store(Request $request)
   {
-    $request->validate([
-      'id' => 'required|integer',
-      'work_order_id' => 'required|integer',
-      'bank_id' => 'nullable|integer',
-      'purpose_id' => 'required|integer',
-      'weather_id' => 'nullable|integer',
-      'priority_id' => 'required|integer|min:1|max:3',
-      'status_id' => 'required|integer|min:1|max:5',
-      'sample_time' => 'required',
-      'sample_description' => 'required|min:5|max:250',
-      'sample_location' => 'nullable|min:5|max:100',
-      'road_name' => 'nullable|min:5|max:100',
-      'road_station_start' => 'nullable|max:11',
-      'road_station_end' => 'nullable|max:11',
-      'road_station' => 'nullable|max:11',
-      'road_body' => 'nullable|max:20',
-      'road_side' => 'nullable|max:10',
-      'phreatic_level' => 'nullable|max:4',
-      'sampling_seq' => 'nullable|numeric|min:1',
-      'env_temp' => 'nullable|numeric|min:1|max:50',
-      'sample_seq' => 'nullable|numeric|min:1',
-      'sample_tests' => 'nullable|max:100',
-      'sample_notes' => 'nullable|max:500',
-      'sample_receipt_date' => 'required|date|before:tomorrow',
-      'sketch_file' => 'nullable|url|max:50',
-      'stratigraphic_file' => 'nullable|url|max:50'
-    ]);
+    (new Sample)->isValid($request);
 
     $sample = new Sample();
-    $sample->id = $request->get('id');
-    $sample->work_order_id = $request->get('work_order_id');
-    $sample->bank_id = $request->get('bank_id');
-    $sample->purpose_id = $request->get('purpose_id');
-    $sample->weather_id = $request->get('weather_id');
-    $sample->priority_id = $request->get('priority_id');
-    $sample->status_id = $request->get('status_id');
-    $sample->sample_time = $request->get('sample_time');
-    $sample->sample_description = $request->get('sample_description');
-    $sample->sample_location = $request->get('sample_location');
-    $sample->road_name = $request->get('road_name');
-    $sample->road_station_start = $request->get('road_station_start');
-    $sample->road_station_end = $request->get('road_station_end');
-    $sample->road_station = $request->get('road_station');
-    $sample->road_body = $request->get('road_body');
-    $sample->road_side = $request->get('road_side');
-    $sample->phreatic_level = $request->get('phreatic_level');
-    $sample->sampling_seq = $request->get('sampling_seq');
-    $sample->env_temp = $request->get('env_temp');
-    $sample->sample_seq = $request->get('sample_seq');
-    $sample->sample_tests = $request->get('sample_tests');;
-    $sample->sample_notes = $request->get('sample_notes');
-    $sample->sample_receipt_date = $request->get('sample_receipt_date');
-    $sample->sample_loc_x = $request->get('sample_loc_x');
-    $sample->sample_loc_y = $request->get('sample_loc_y');
-    $sample->sketch_file = $request->get('sketch_file');
-    $sample->stratigraphic_file = $request->get('stratigraphic_file');
-    $sample->save();
+    
+    (new Sample)->saveSample($request, $sample);
 
     return back()->withInput()->with('status', 'Registro guardado exitosamente');
   }
@@ -153,48 +76,79 @@ class SampleController extends Controller
   /**
    * Display the specified resource.
    *
-   * @param Sample $sample
-   * @return void
+   * @param int $id
+   * @return Response
    */
-  public function show(Sample $sample)
+  public function show(int $id)
   {
-    //
+    $sample = (new Sample)->showSample($id);
+
+    return view('workOrders.show', [
+      'sample' => $sample
+    ]);
   }
 
   /**
    * Show the form for editing the specified resource.
    *
-   * @param Sample $sample
-   * @return void
+   * @param int $id
+   * @return Response
    */
-  public function edit(Sample $sample)
+  public function edit(int $id)
   {
-    //
+    $sample = (new Sample)->showSample($id);
+
+    $workOrders = (new WorkOrder)->getWorkOrders();
+
+    $banks = (new Bank)->getBanks();
+
+    $purposes = (new Purpose)->getPurposes();
+
+    $weathers = (new Wather)->getWeather();
+
+    $priorities = (new Priority)->getPriority();
+
+    $statuses = (new Status)->getStatuses();
+
+    return view('samples.edit', [
+      'sample' => $sample,
+      'workOrders' => $workOrders,
+      'banks' => $banks,
+      'purposes' => $purposes,
+      'weathers' => $weathers,
+      'priorities' => $priorities,
+      'statuses' => $statuses
+    ]);
   }
 
   /**
    * Update the specified resource in storage.
    *
    * @param Request $request
-   * @param Sample $sample
-   * @return void
+   * @param int $id
+   * @return Response
    */
-  public function update(Request $request, Sample $sample)
+  public function update(Request $request, int $id)
   {
-    //
+    (new Sample)->isValid($request);
+
+    $sample = (new Sample)->showSample($id);
+
+    (new Sample)->saveSample($request, $sample);
+
+    return back()->withInput()->with('status', 'Registro actualizado exitosamente');
   }
 
   /**
    * Remove the specified resource from storage.
    *
-   * @param Sample $sample
+   * @param int $id
    * @return Response
-   * @throws Exception
    */
-  public function destroy(Sample $sample)
+  public function destroy(int $id)
   {
-    $sample->delete();
+    (new Sample)->deleteSample($id);
 
-    return redirect('/samples');
+    return redirect('/samples')->with('status', 'Registro eliminado exitosamente');
   }
 }
