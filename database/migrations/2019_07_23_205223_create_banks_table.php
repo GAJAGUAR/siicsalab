25117785b10563b496bffe51675b81d98b0d50a5
G.A.JAGUAR <dev.gajaguar@gmail.com>
2019-10-04 10:08:44 -0500

    Add more precision to 'banks' location

--- database/migrations/2019_07_23_205223_create_banks_table.php
+++ database/migrations/2019_07_23_205223_create_banks_table.php
@@ -22,7 +22,34 @@ class CreateBanksTable extends Migration
       $table->collation = 'utf8mb4_spanish_ci';
       $table->smallIncrements('id');
       $table->string('bank_name', 50);
+      $table->string('road_name', 100)
+        ->nullable()
+        ->default(null);
+      $table->string('road_station', 11)
+        ->nullable()
+        ->default(null);
+      $table->string('road_side', 20)
+        ->nullable()
+        ->default(null);
+      $table->string('road_complement', 100)
+        ->nullable()
+        ->default(null);
+      $table->string('road', 200)
+        ->virtualAs('
+          CONCAT(
+            IF(`road_name` <> "", `road_name`, ""),
+            IF(`road_station` <> "", CONCAT(" KM ", `road_station`), ""),
+            IF(`road_side` <> "", CONCAT(" LADO ",`road_side`), ""),
+            IF(`road_complement` <> "", CONCAT(" ", `road_complement`), "")
+          )
+        ');
       $table->string('bank_location', 500);
+      $table->decimal('bank_latitude', 9, 6)
+        ->nullable()
+        ->default(null);
+      $table->decimal('bank_longitude', 9, 6)
+        ->nullable()
+        ->default(null);
       $table->timestamps();
 
       // Indexes
